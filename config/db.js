f46101d81af50a29b8f55aa5fce092ad86de528e
Atta khan <atta@mediamindmail.com>
2025-11-15 13:19:50 +0100

    integrate dotenv

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool ({
-    user: "attakhan",
-    host: "localhost",
-    database: "node_app",
-    password: "password123",
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
 pool.connect()
